ea955c07c8e3e7e8f30f7c88ac3b7a66844ae717
Bokjihee <bokjihee@naver.com>
2024-11-26 18:00:38 +0900

    refactor: 선생님 학생 연결 관련 에러 수정

--- src/main/java/com/tutorpus/tutorpus/connect/service/ConnectService.java
+++ src/main/java/com/tutorpus/tutorpus/connect/service/ConnectService.java
@@ -6,9 +6,13 @@ import com.tutorpus.tutorpus.connect.entity.Connect;
 import com.tutorpus.tutorpus.connect.entity.Day;
 import com.tutorpus.tutorpus.connect.repository.ClassDayRepository;
 import com.tutorpus.tutorpus.connect.repository.ConnectRepository;
+import com.tutorpus.tutorpus.exception.CustomException;
+import com.tutorpus.tutorpus.exception.ErrorCode;
 import com.tutorpus.tutorpus.member.entity.Member;
+import com.tutorpus.tutorpus.member.entity.Role;
 import com.tutorpus.tutorpus.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,9 +25,11 @@ public class ConnectService {
 
     @Transactional
     public void teacherStudentConnect(ConnectRequestDto connectDto, Member teacher) {
-        //학생
-        //TODO: 에러 던지기
-        Member student = memberRepository.findByEmail(connectDto.getStudentEmail()).orElse(null);
+        Member student = memberRepository.findByEmail(connectDto.getStudentEmail())
+                .orElseThrow(() -> new CustomException(ErrorCode.NO_MEMBER));
+        //선생님이 학생만 저장할 수 있음. 그외의 경우 에러
+        if (student.getRole() != Role.STUDENT) throw new CustomException(ErrorCode.NOT_STUDENT);
+        if (teacher.getRole() != Role.TEACHER) throw new CustomException(ErrorCode.NOT_TEACHER);
 
         //선생님과 학생 연결정보
         Connect connect = Connect.builder()
