617cb5d8fe8f062e8844be6c30e1aea386b96a05
Bokjihee <bokjihee@naver.com>
2024-12-15 23:46:33 +0900

    feat: 해당 날짜의 숙제 전체 조회

--- src/main/java/com/tutorpus/tutorpus/homework/repository/HomeworkRepository.java
+++ src/main/java/com/tutorpus/tutorpus/homework/repository/HomeworkRepository.java
@@ -2,6 +2,18 @@ package com.tutorpus.tutorpus.homework.repository;
 
 import com.tutorpus.tutorpus.homework.entity.Homework;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface HomeworkRepository extends JpaRepository<Homework, Long> {
+    @Query(value = "SELECT * FROM homework h " +
+            "WHERE h.connect_id = :connectId " +
+            "AND h.class_date BETWEEN :startOfDay AND :endOfDay",
+            nativeQuery = true)
+    List<Homework> findByConnectIdAndDate(@Param("connectId") Long connectId,
+                                          @Param("startOfDay") LocalDateTime startOfDay,
+                                          @Param("endOfDay") LocalDateTime endOfDay);
 }
--- src/main/java/com/tutorpus/tutorpus/homework/service/HomeworkService.java
+++ src/main/java/com/tutorpus/tutorpus/homework/service/HomeworkService.java
@@ -4,13 +4,20 @@ import com.tutorpus.tutorpus.connect.entity.Connect;
 import com.tutorpus.tutorpus.connect.repository.ConnectRepository;
 import com.tutorpus.tutorpus.exception.CustomException;
 import com.tutorpus.tutorpus.exception.ErrorCode;
-import com.tutorpus.tutorpus.homework.dto.addHomeworkDto;
+import com.tutorpus.tutorpus.homework.dto.AddHomeworkDto;
+import com.tutorpus.tutorpus.homework.dto.ReturnHomeworkDto;
 import com.tutorpus.tutorpus.homework.entity.Homework;
 import com.tutorpus.tutorpus.homework.repository.HomeworkRepository;
 import com.tutorpus.tutorpus.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -19,7 +26,7 @@ public class HomeworkService {
     private final ConnectRepository connectRepository;
 
     @Transactional
-    public void addHomework(Member member, addHomeworkDto dto) {
+    public void addHomework(Member member, AddHomeworkDto dto) {
         Connect connect = connectRepository.findById(dto.getConnectId())
                 .orElseThrow(()-> new CustomException(ErrorCode.NO_CONNECT_ID));
         //connect의 선생님 정보와 member 정보가 일치하지 않는 경우
@@ -35,5 +42,29 @@ public class HomeworkService {
                 .done(false)
                 .build();
         homeworkRepository.save(homework);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReturnHomeworkDto> getHomework(Member member, Long connectId, LocalDate date) {
+        Connect connect = connectRepository.findById(connectId)
+                .orElseThrow(()-> new CustomException(ErrorCode.NO_CONNECT_ID));
+        if((connect.getTeacher().getId() != member.getId()) || (connect.getStudent().getId() != member.getId()))
+            throw new CustomException(ErrorCode.NO_CORRECT_CONNECT_ID);
+
+        //LocalDate(선택날짜) -> LocalDateTime 변환
+        LocalDateTime startOfDay = date.atStartOfDay();
+        LocalDateTime endOfDay = date.atTime(LocalTime.MAX);
+
+        List<ReturnHomeworkDto> returnDto = homeworkRepository.findByConnectIdAndDate(connectId, startOfDay, endOfDay).stream()
+                .map(homework -> ReturnHomeworkDto.builder()
+                        .homeworkId(homework.getId())
+                        .title(homework.getTitle())
+                        .content(homework.getContent())
+                        .dueDate(homework.getEndDate())
+                        .done(homework.getDone())
+                        .build()
+                )
+                .collect(Collectors.toList());
+        return returnDto;
     }
 }
